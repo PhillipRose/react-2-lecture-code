5c609c9285ee0ee0e5186eefd65f45804ccd3c39
PhillipRose <philliprosejob@gmail.com>
2020-09-17 12:00:07 -0600

    react 2 leacture with my own notes

--- src/App.js
+++ src/App.js
@@ -10,13 +10,48 @@ class App extends Component {
       username: 'Matt',
       todoList: ['Code the things', 'Eat', 'Sleep']
     }
+    //inside construtor .bind prevents errors later.
+    //set to variable and .bind this to the function.
+    //keeps this. as this.App meaning this.state object.
+    this.changeUsername = this. changeUsername.bind(this);
+  }
+  
+  changeUsername(){
+    this.setState({username: 'Eric'})
   }
 
+  //Public Classfield Syntax
+  //Arrow function to workaround .bind
+  properContext = ()=>{
+    this.setState({username: 'Aaron'})
+  }
+  
+  //any data passed into the Component portion of the render return is accessable with props ie username = or greeting = 
+  
   render(){
+    // const mappedTodos = this.state.todoList.map((todo, i)=> {
+    //   return <h2 key={i}>{todo}</h2>
+    // })
+    const mappedTodos = this.state.todoList.map((todo, i)=> {
+      return(
+        // ListDisplay is Component key and todo are props for ListDisplay
+      <ListDisplay key={i} todo={todo}/> )
+      
+    })
+  
     return (
       <div className="App">
-        <Header />
+        <Header username={this.state.username} greeting='Hello World' usernameFn = {this.changeUsername} secondUsername ={this.properContext} />
         <h1>My Todo List</h1>
+        {mappedTodos}
+        {/* Alternate to {mappedTodos} method */}
+
+        {/* {this.state.todoList.map((todo, i) => (
+         
+            <ListDisplay key={i} todo={todo}/>
+          )
+        )} */}
+
       </div>
     )
   }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
-const Header = () => {
+//functional components need to be passed props as an argument to receive them. Place 'props ' as a parameter  to work correctly
+const Header = (props) => {
+    //console.log(props) to see what you have to work with.
+    console.log(props)
+
+
     return (
-        <header className='main-header'></header>
+        <header className='main-header'>
+            <h1>{props.username} says {props.greeting}</h1>
+            <button onClick={props.usernameFn}>Change Username</button>
+            {/* Public Classfield Syntax to not use .bind, button example */}
+            <button onClick={props.secondUsername}>Change Username Again</button>
+        </header>
     )
 }
 
